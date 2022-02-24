401725b36650fe796a46a8df51ab0e51556a5085
RossiniLorenzo99 <rossini.lorenzo@iisgalvanimi.edu.it>
2022-02-24 09:00:50 +0000

    Da Finire

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'angular-youtube-player-RossiniLorenzo99';
+  ngOnInit() {
+    // const tag = document.createElement("script");
+    // tag.src = "https://www.youtube.com/iframe_api";
+    // document.body.appendChild(tag);
+  }
+
+}
+
